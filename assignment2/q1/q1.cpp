62a0906a72667dfb924bc78d5f700155869181ba
harinireddy30 <37109315+harinireddy30@users.noreply.github.com>
2019-02-21 10:14:23 +0530

    made changes to prgm1

--- assignment2/q1/q1.cpp
+++ assignment2/q1/q1.cpp
@@ -1,31 +1,37 @@
 #include<iostream>
 using namespace std;
 class polar {
-	int x;
+	int r;
+	int angle;
 	static int count;
 public:
-	void c()
+	polar()
 	{
-		x=++count;
+		r = angle = 0;
 	}
-	void objno()
+	polar(int x, int y)
 	{
-		cout << "The object number is" << x << endl;
+		r = x;
+		angle = y;
 	}
-	static void display()
+	static int get_count()
 	{
-		cout << "The no of objects created is" << count << endl;
+		return count;
+	}
+	static void inc_count()
+	{
+		++count;
 	}
 };
 int polar::count;
 void main()
 {
 	int y;
-	polar p1, p2;
-	p1.c();
-	p2.c(); 
-	polar::display;
-	p1.objno();
-	p2.objno();
+	polar p1,p2;
+	p1=polar();
+	p2=polar(1, 2);
+	p1.inc_count();
+	p2.inc_count();
+	cout << polar::get_count() << endl;
 	cin >> y;
 }
